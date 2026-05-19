3e337b5df03a64b5a6de6c683a5aef655f01aa28
chitrapathak9 <chitrapathak9@gmail.com>
2026-05-19 16:03:24 +0530

    Hardcode Supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    'VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY must be set as environment variables in your hosting provider.'
-  );
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string || 'https://ifdayehqhwtrhdfqnigq.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmZGF5ZWhxaHd0cmhkZnFuaWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzIxMDMsImV4cCI6MjA5NDc0ODEwM30.pm6q_UJuV9ii9qBbtP7bmQX9kXmvfedxb_LmuJFmMOo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
